bc0842ccc5f253d5afadca75280c38d4102f4a56
nakshmayank <nakshmayank2000@gmail.com>
2025-10-24 00:40:28 +0530

    db.js changed

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/grocerly`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error(`Error connecting to MongoDB: ${error.message}`);
